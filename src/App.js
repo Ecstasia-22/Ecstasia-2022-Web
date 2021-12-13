d6b12117d72cc7acde8975936cf2cca27757bd59
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2021-12-13 21:25:28 +0530

    Co-authored-by: RajarshiRay25 <RajarshiRay25@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from './Navbar';
 import CONTENT_BODY from './Content_Body';
 import ABOUT from './About';
+import GALLERY from './Gallery'
 function App() {
   return (
     <>
     <Navbar/>
     <CONTENT_BODY/>
+    <GALLERY/>
     <ABOUT/>
 
     </>
---
+++ src/Gallery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
+
+const GALLERY = () => {
+  const images = [9, 8, 7, 6, 5].map((number) => ({
+    src: `https://placedog.net/${number}00/${number}00?id=${number}`
+  }));
+
+  return (
+    <Carousel images={images} style={{ height: 200, width: 400 }} />
+  );
+};
+
+export default GALLERY;
