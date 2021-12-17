7b5c322c38307bb2cd5eca3f2b5b5909f5ef7291
RajarshiRay25 <rayrajarshi9@gmail.com>
2021-12-17 21:04:08 +0530

    feat: flex wrap responsive

---
+++ src/Gallery.js
@@ -0,0 +1,13 @@
+const Gallery = () => {
+    return ( 
+        <>
+        <div className="head"><h3>Gallery</h3></div>
+        <div className="pics">
+            <p>pictures Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, est molestiae omnis quis beatae repudiandae aperiam culpa minima suscipit commodi dolores sed unde? Amet rerum, nostrum reprehenderit dolores atque dolor?
+            </p>
+        </div>
+        </>
+     );
+}
+ 
+export default Gallery;
