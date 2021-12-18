9da9b360fdc224e03547894f9f1830ae1dd5ad96
RajarshiRay25 <RajarshiRay25@users.noreply.github.com>
2021-12-18 17:06:59 +0000

    (CI) Apply formatting changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar';
-import CONTENT_BODY from './Content_Body';
-import ABOUT from './About';
-import Gallery from './Gallery';
+import logo from './logo.svg'
+import './App.css'
+import Navbar from './Navbar'
+import CONTENT_BODY from './Content_Body'
+import ABOUT from './About'
+import Gallery from './Gallery'
 function App() {
   return (
     <>
@@ -11,7 +11,6 @@ function App() {
       <CONTENT_BODY />
       <Gallery />
       <ABOUT />
-
     </>
   )
 }
