ad0804bcba6592bc2967f5060967c27f14e9bd2a
MazumdarAyush07 <MazumdarAyush07@users.noreply.github.com>
2022-01-02 04:42:20 +0000

    (CI) Apply formatting changes

--- src/components/Navbar/MenuItems.jsx
+++ src/components/Navbar/MenuItems.jsx
@@ -1,39 +1,37 @@
 export const MenuItems = [
-
-    {
-        title:'Home',
-        url:'#',
-        cName:'nav-links'
-    },
-    {
-        title:'Events',
-        url:'#',
-        cName:'nav-links'
-    },
-    {
-        title:'Gallery',
-        url:'#',
-        cName:'nav-links'
-    },
-    {
-        title:'Partners',
-        url:'#',
-        cName:'nav-links'
-    },
-    {
-        title:'About Us',
-        url:'#',
-        cName:'nav-links'
-    },
-    {
-        title:'Contact Us',
-        url:'#',
-        cName: 'nav-links'
-    },
-    {
-        title:'News Feed',
-        url:'#',
-        cName:'nav-links'
-    }
-    
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Events',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Gallery',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Partners',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'About Us',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Contact Us',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'News Feed',
+    url: '#',
+    cName: 'nav-links'
+  }
 ]
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import './home.css'
 import Navbar from '../../components/Navbar'
 function Home() {
-  return <>
-
-  <Navbar></Navbar>
-  
-  
-  </>
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
 }
 
 export default Home
