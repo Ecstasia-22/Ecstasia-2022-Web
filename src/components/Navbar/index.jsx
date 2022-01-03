27fd4464833584ba0ac9176850b62d5d673c4c0e
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2022-01-03 18:11:12 +0530

    Add: responsive navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,13 +6,15 @@ class Navbar extends Component {
   state = { clicked: false }
 
   handleClick = () => {
-    this.setState({ clicked: !this.state.clicked })
-  }
+      this.setState({ clicked: !this.state.clicked })
+}
 
   render() {
     return (
       <nav className="NavbarItems">
-        <div className="menu-icon"></div>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
         <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
           {MenuItems.map((item, index) => {
             return (
