ccaf195dd2d20957a8dd041c2cfdbdca5e78d8e2
MazumdarAyush07 <MazumdarAyush07@users.noreply.github.com>
2022-01-03 12:41:54 +0000

    (CI) Apply formatting changes

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,8 +6,8 @@ class Navbar extends Component {
   state = { clicked: false }
 
   handleClick = () => {
-      this.setState({ clicked: !this.state.clicked })
-}
+    this.setState({ clicked: !this.state.clicked })
+  }
 
   render() {
     return (
