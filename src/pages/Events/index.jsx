94394be3c8237060c53b88be5ea76772554346ad
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2022-01-06 21:50:18 +0530

    Add: Events Page

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -1,7 +1,30 @@
-import './style.css'
-
+import './style.css';
+import EventsBG from "../../Assets/EventsBG.png"
+import Dance from "../../Assets/Dance.png"
+import Drama from "../../Assets/Drama.png"
+import FineArts from "../../Assets/FineArts.png"
+import Games from "../../Assets/Games.png"
+import Informals from "../../Assets/Informals.png"
+import Literature from "../../Assets/Literature.png"
+import Music from "../../Assets/Music.png"
+import Photography from "../../Assets/Photography.png"
 function Events() {
-  return <>Events page</>
+  return( 
+  <>
+  <div className="background" style={{backgroundImage:`url(${EventsBG})`}}> 
+    <div className="card">
+        <img src={Dance} alt="Dance"/>
+        <img src={Music} alt="Music"/>
+        <img src={Drama} alt="Drama"/>
+        <img src={Photography} alt="Photography"/>
+        <img src={Literature} alt="Literature"/>
+        <img src={Games} alt="Games"/>
+        <img src={FineArts} alt="FineArts"/>
+        <img src={Informals} alt="Informals"/>
+    </div>
+  </div>  
+  </>
+  )
 }
 
 export default Events
