9a56ef227fbac32f30cd973011b032f46742c02a
swetakarar <kararsweta@gmail.com>
2022-03-16 22:36:25 +0530

    add:about us and footer wireframe

--- src/components/Home/AboutUs/index.jsx
+++ src/components/Home/AboutUs/index.jsx
@@ -1,7 +1,32 @@
+import { Grid, Box, Item, Typography } from '@mui/material'
 import React from 'react'
+import './aboutUs.css'
+import ecstasiaLogo from './Logo.png'
 
 export default function About() {
   return (
-    <div>About</div>
+    <Box
+      sx={{
+        display: 'grid',
+        gap: 1,
+        gridTemplateColumns: 'repeat(2, 1fr)'
+      }}
+    >
+      <Grid>
+        <img src={ecstasiaLogo} alt="logo" />
+      </Grid>
+      <Grid>
+        <Typography>About Us</Typography>
+        <Typography>
+          ECSTASIA is a land of celebration of talents. It is a cultural fest where
+          artists and players gather around to share their expertise with everyone.
+          Ecstasia is filled with a lot of activities like games and sports, music, drama,
+          dance, literacy events, fine arts, photography and even online competitions. It
+          is the celebration of uniqueness. Join us on the 4th edition of Ecstasia, March
+          2022, organized by UEM Kolkata. Let us all gather and make this a significant
+          moment of our life.
+        </Typography>
+      </Grid>
+    </Box>
   )
 }
--- src/components/Home/Footer/index.jsx
+++ src/components/Home/Footer/index.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material'
+
+import './footer.css'
+
+import ecstasiaLogo from './Logo.png'
+import map from './map.png'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <Box className="footerMain" sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          <Grid className="logoclass" item xs={2} sm={4} md={4}>
+            <img src={ecstasiaLogo} alt="logo" />
+          </Grid>
+          <Grid classname="typoTexts" item xs={2} sm={4} md={4}>
+            <Typography>For General Enquiries: </Typography>
+            <Typography>ecstasia.uemk@gmail.com</Typography>
+            <br />
+            <br />
+            <Typography>For Sponorship Enquiries:</Typography>
+            <Typography>sponsorship.ecstasia.uemk@gmail.com</Typography>
+          </Grid>
+
+          <Grid className="map" item xs={2} sm={4} md={4}>
+            <img src={map} alt="imgmap" />
+          </Grid>
+          <hr />
+          <br />
+        </Grid>
+      </Box>
+    </>
   )
 }
