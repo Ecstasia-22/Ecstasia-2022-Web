3ebb00870134b66f550e8bc1d9c123573dc12b9e
swetakarar <kararsweta@gmail.com>
2022-03-16 22:43:45 +0530

    fix: minor

--- src/components/Home/Footer/index.jsx
+++ src/components/Home/Footer/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 import { Typography } from '@mui/material'
 
@@ -35,5 +34,4 @@ export default function Footer() {
       </Box>
     </>
   )
-  return <div>Footer</div>
 }
