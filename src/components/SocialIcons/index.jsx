87492fadb0295a0090793b36e0206bb235cb9477
Prasundas99 <prasundas115@gmail.com>
2022-03-17 00:13:31 +0530

    feat: social icons

--- src/components/SocialIcons/index.jsx
+++ src/components/SocialIcons/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./social.css"
+import Facebook from '../../Assets/socials/facebook.svg'
+import Instagram from '../../Assets/socials/instagram.svg'
+import Twitter from '../../Assets/socials/twitter.svg'
+import Youtube from '../../Assets/socials/youtube.svg'
+
+
 export default function SocialIcons() {
   return (
     <div className='sticky-social'>
-    asdfghjkjhgfdcvbnjuygtfvbjnhgfds
+        <div className='icons'>
+        <img src={Facebook} alt="facebook" className='social-icon'/>
+        <img src={Twitter} alt="twitter" className='social-icon'/>
+        <img src={Instagram} alt="instagram" className='social-icon'/>
+        <img src={Youtube} alt="youtube" className='social-icon'/>
+        </div>
     </div>
   )
 }
