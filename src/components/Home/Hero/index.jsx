650fc1827844fcff5fe005bfb380e9bfaf635b6a
Prasundas99 <prasundas115@gmail.com>
2022-03-17 00:19:55 +0530

    feat: hero section

--- src/components/Home/Hero/index.jsx
+++ src/components/Home/Hero/index.jsx
@@ -2,6 +2,7 @@ import { Container, Typography } from '@mui/material'
 import React from 'react'
 import './hero.css'
 import BgVideo from '../../../Assets/Home/hero.mp4'
+import Arrow from '../../../Assets/Home/arrow.svg'
 
 export default function Hero() {
   return (<div className='hero'>
@@ -14,7 +15,7 @@ export default function Hero() {
         <h2 className='hero_h2' >ANNUAL CULTURAL FESTIVAL OF UEM KOLKATA</h2>
       </div>
       <Typography align="center">
-
+          <img src={Arrow} alt="arrow" className='hero_arrow'/>
       </Typography>
     </Container>
   </div>)
