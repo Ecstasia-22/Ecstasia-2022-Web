7e7215c3f3cab987d64a39d90b9f27286aab50b1
Prasundas99 <prasundas115@gmail.com>
2022-03-16 21:14:51 +0530

    feat: home page components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,15 @@
+import About from '../../components/Home/AboutUs/index.jsx'
+import Footer from '../../components/Home/Footer/index.jsx'
+import Gallery from '../../components/Home/Gallery/index.jsx'
+import Hero from '../../components/Home/Hero/index.jsx'
 import './home.css'
 function Home() {
-  return <>Home page</>
+  return <>
+  <Hero />
+  <About />
+  <Gallery />
+  <Footer />
+  </>
 }
 
 export default Home
