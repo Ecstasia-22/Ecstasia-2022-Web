ddbeb783e7c01b23aa4b1f858590315d75b3896a
Prasundas99 <prasundas115@gmail.com>
2022-03-17 01:13:32 +0530

    feat: new font

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import './Assets/fonts/urbanJungle.otf'
+import './Assets/fonts/UrbanJungleDEMO.woff2'
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
