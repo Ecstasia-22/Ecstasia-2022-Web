ed1015cc2b5a0cd200489bd5106210bb818d8c32
Prasundas99 <prasundas115@gmail.com>
2022-03-17 23:38:59 +0530

    fix: font issues

--- src/components/Home/AboutUs/index.jsx
+++ src/components/Home/AboutUs/index.jsx
@@ -8,15 +8,17 @@ export default function About() {
     <div className="about">
       <Container maxWidth="xl">
         <Grid container>
-          <Grid item md={4} className="logoClass">
-            <img src={ecstasiaLogo} alt="logo" />
+          <Grid item md={4} xs={12} >
+          <Typography align="center">
+            <img src={ecstasiaLogo} className="logoClass" alt="logo" />
+            </Typography>
           </Grid>
-          <Grid item md={8} className="contentClass">
+          <Grid item md={8} xs={12} className="contentClass">
             <Typography variant="h4" align="center">
-              <b>About Us</b>
+              <b style={{fontWeight: '800'}}>About Us</b>
             </Typography>
             <br />
-            <Typography variant="h6">
+            <Typography variant="h6" align="left">
               ECSTASIA is a land of celebration of talents. It is a cultural fest where
               artists and players gather around to share their expertise with everyone.
               Ecstasia is filled with a lot of activities like games and sports, music,
