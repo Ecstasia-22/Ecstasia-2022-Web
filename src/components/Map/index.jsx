6c9dbfa1944bdb07420427dfd8ae93228e4b46a6
swetakarar <kararsweta@gmail.com>
2022-03-17 21:55:36 +0530

    add: copyright of footer

---
+++ src/components/Map/index.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>
+
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 10
+  }
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '220px', width: '400px' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBoL0--NTsQmAU2qVix4Uzh0Mr1K046ty0' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        </GoogleMapReact>
+      </div>
+    )
+  }
+}
+
+export default Map
