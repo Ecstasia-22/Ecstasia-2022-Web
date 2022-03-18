572419a7ff2cb6e5ed7d8207179d092c1f72afae
Prasundas99 <prasundas115@gmail.com>
2022-03-18 22:14:01 +0530

    feat: json for single event

---
+++ src/pages/EventDetail/eventDetail.js
@@ -0,0 +1,19 @@
+export const singleEvent = [
+    {
+        id: 1,
+        params: '',
+        title: 'Music',
+        subtitle: 'https://i.ibb.co/0zZxXxL/cover.jpg',
+        desc: 'https://i.ibb.co/0zZxXxL/watermark.png',
+        rules: 'https://i.ibb.co/0zZxXxL/watermark.png',
+        coOrdinators: [
+            {
+                name: 'Saurabh',
+                number: '+91-9588888888',
+            }
+        ],
+        coverImage: 'https://i.ibb.co/0zZxXxL/cover.jpg',
+        coverColor: '#121013',
+        regLink: 'https://www.google.com/',
+    }
+]
