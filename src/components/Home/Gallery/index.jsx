f83e3a66208926d0776e95b892dce5715c0e53f6
Prasundas99 <Prasundas99@users.noreply.github.com>
2022-03-18 15:23:59 +0000

    (CI) Apply formatting changes

--- src/components/Home/Gallery/index.jsx
+++ src/components/Home/Gallery/index.jsx
@@ -4,17 +4,17 @@ import './sectionGallery.css'
 import { Typography, Button } from '@mui/material'
 
 const images = [
-  "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
-  "https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg",
-  "https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg",
-  "https://66.media.tumblr.com/5516a22e0cdacaa85311ec3f8fd1e9ef/tumblr_o45jwvdsL11qho82wo1_1280.jpg",
-  "https://66.media.tumblr.com/f19901f50b79604839ca761cd6d74748/tumblr_o65rohhkQL1qho82wo1_1280.jpg"
+  'https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg',
+  'https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg',
+  'https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg',
+  'https://66.media.tumblr.com/5516a22e0cdacaa85311ec3f8fd1e9ef/tumblr_o45jwvdsL11qho82wo1_1280.jpg',
+  'https://66.media.tumblr.com/f19901f50b79604839ca761cd6d74748/tumblr_o65rohhkQL1qho82wo1_1280.jpg'
 ]
 
 const Card = ({ image, active, ...rest }) => {
   return (
     <div
-      className={`option ${active && "active"}`}
+      className={`option ${active && 'active'}`}
       style={{
         background: `url(${image})`
       }}
@@ -41,7 +41,7 @@ export default function Gallery() {
       <Typography className="textG" variant="h4" align="center">
         Gallery
       </Typography>
-      <Typography align="center" className='sectionGallery'>
+      <Typography align="center" className="sectionGallery">
         <div className="options">
           {images.map((img, i) => (
             <Card
@@ -52,7 +52,7 @@ export default function Gallery() {
             />
           ))}
         </div>
-      </Typography >
+      </Typography>
     </div>
   )
 }
