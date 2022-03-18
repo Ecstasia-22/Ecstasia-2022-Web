c41a2534d0bed51c85e88838836379a1877aff64
Prasundas99 <Prasundas99@users.noreply.github.com>
2022-03-18 15:38:26 +0000

    (CI) Apply formatting changes

--- src/components/Home/Gallery/index.jsx
+++ src/components/Home/Gallery/index.jsx
@@ -3,19 +3,18 @@ import './sectionGallery.css'
 import { Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 
-
 const images = [
-  "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
-  "https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg",
-  "https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg",
-  "https://66.media.tumblr.com/5516a22e0cdacaa85311ec3f8fd1e9ef/tumblr_o45jwvdsL11qho82wo1_1280.jpg",
-  "https://66.media.tumblr.com/f19901f50b79604839ca761cd6d74748/tumblr_o65rohhkQL1qho82wo1_1280.jpg"
+  'https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg',
+  'https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg',
+  'https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg',
+  'https://66.media.tumblr.com/5516a22e0cdacaa85311ec3f8fd1e9ef/tumblr_o45jwvdsL11qho82wo1_1280.jpg',
+  'https://66.media.tumblr.com/f19901f50b79604839ca761cd6d74748/tumblr_o65rohhkQL1qho82wo1_1280.jpg'
 ]
 
 const Card = ({ image, active, ...rest }) => {
   return (
     <div
-      className={`option ${active && "active"}`}
+      className={`option ${active && 'active'}`}
       style={{
         background: `url(${image})`
       }}
@@ -42,7 +41,7 @@ export default function Gallery() {
       <Typography className="textG" variant="h4" align="center">
         Gallery
       </Typography>
-      <Typography align="center" className='sectionGallery'>
+      <Typography align="center" className="sectionGallery">
         <div className="options">
           {images.map((img, i) => (
             <Card
@@ -53,11 +52,11 @@ export default function Gallery() {
             />
           ))}
         </div>
-      </Typography >
+      </Typography>
 
       <Typography align="center">
-        <Link to={"/gallery"} >
-          <button className='sectionGallery_btn'> View More </button>
+        <Link to={'/gallery'}>
+          <button className="sectionGallery_btn"> View More </button>
         </Link>
       </Typography>
     </div>
--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -7,7 +7,7 @@ class Map extends Component {
       <div style={{ height: '220px', width: '100%' }}>
         <div className="google-map-code">
           <iframe
-            title='UEM Kolkata'
+            title="UEM Kolkata"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.512695709607!2d88.48771271492477!3d22.559920185189334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a020b267a3cdc13%3A0xb3b21d652126f40!2sUniversity%20of%20Engineering%20%26%20Management%20(UEM)%2C%20Kolkata!5e0!3m2!1sen!2sin!4v1647544363590!5m2!1sen!2sin"
             width="100%"
             height="220"
