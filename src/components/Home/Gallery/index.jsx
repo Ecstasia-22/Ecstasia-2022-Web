81834b30ada77fb9fb2f6b380972482ea7e137f1
swetakarar <kararsweta@gmail.com>
2022-03-20 01:40:37 +0530

    fix:home button

--- src/components/Home/Gallery/index.jsx
+++ src/components/Home/Gallery/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './sectionGallery.css'
-import { Typography } from '@mui/material'
+import { Typography, Grid } from '@mui/material'
 import { Link } from 'react-router-dom'
 import img from './e1.jpg'
 import img2 from './e2.jpg'
@@ -53,7 +53,9 @@ export default function Gallery() {
 
       <Typography align="center">
         <Link to={'/gallery'}>
-          <button className="sectionGallery_btn"> View More </button>
+          <Grid align="center">
+            <button className="sectionGallery_btn"> View More </button>
+          </Grid>
         </Link>
       </Typography>
     </div>
