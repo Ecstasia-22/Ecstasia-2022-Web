1773d27d6c9c4bc0ae97b4ac303e0d787a02cac4
RajarshiRay25 <rayrajarshi9@gmail.com>
2022-03-19 16:58:29 +0530

    Home Page Gallery slider pics given(fit issue)+Gallery page pics added

--- src/pages/Gallery/index copy.jsx
+++ src/pages/Gallery/index copy.jsx
@@ -2,40 +2,40 @@ import React from 'react'
 import { Container, Grid } from '@mui/material'
 import './gallery.css'
 import Footer from '../../components/Home/Footer'
-import img from './e1.jpg'
-import img2 from './e2.jpg'
-import img3 from './e3.jpg'
-import img4 from './e4.jpg'
-import img5 from './e5.jpg'
-import img6 from './e6.jpg'
+
 const galleryArray = [
   {
-    image: img,
+    image:
+      'https://images.unsplash.com/photo-1633994092151-60c9092c2ace?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
     title: 'Image 1',
     Id: 1
   },
   {
-    image: img2,
+    image:
+      'https://media.istockphoto.com/photos/the-musicians-were-playing-rock-music-on-stage-there-was-an-audience-picture-id1319479588?b=1&k=20&m=1319479588&s=170667a&w=0&h=bunblYyTDA_vnXu-nY4x4oa7ke6aiiZKntZ5mfr-4aM=',
     title: 'Image 2',
     Id: 2
   },
   {
-    image: img3,
+    image:
+      'https://images.unsplash.com/photo-1549646075-f533f4622ef7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
     title: 'Image 3',
     Id: 3
   },
   {
-    image: img4,
+    image:
+      'https://images.unsplash.com/photo-1547495827-fff7a5e8b26c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
     title: 'Image 4',
     Id: 4
   },
   {
-    image: img5,
+    image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg',
     title: 'Image 5',
     Id: 5
   },
   {
-    image: img6,
+    image:
+      'https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     title: 'Image 6',
     Id: 6
   }
