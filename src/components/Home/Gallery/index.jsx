a3ffc66bee8cad9167ea0a1cdf33f903b26f9b51
RajarshiRay25 <RajarshiRay25@users.noreply.github.com>
2022-03-19 11:29:39 +0000

    (CI) Apply formatting changes

--- src/components/Home/Gallery/index.jsx
+++ src/components/Home/Gallery/index.jsx
@@ -7,9 +7,7 @@ import img2 from './e2.jpg'
 import img3 from './e3.jpg'
 import img4 from './e4.jpg'
 import img5 from './e5.jpg'
-const images = [
-  img,img2,img3,img4,img5
-]
+const images = [img, img2, img3, img4, img5]
 
 const Card = ({ image, active, ...rest }) => {
   return (
