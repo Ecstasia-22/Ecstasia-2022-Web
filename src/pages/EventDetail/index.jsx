4d51546cd49d331e67ab7e714350e1263e5db8b6
noob <suvrashaw@yahoo.com>
2022-03-20 18:39:26 +0530

    Update index.jsx

--- src/pages/EventDetail/index.jsx
+++ src/pages/EventDetail/index.jsx
@@ -66,9 +66,9 @@ function EventDetail() {
                   Register
                 </button>
               )}
-              </*button className="blurButton">
+              <button class="blurButton __web-inspector-hide-shortcut__">
                 <img src={community} /> 3 and 8 Members per Team{' '}
-              </button*/>
+              </button>
             </div>
           </Grid>
           <Grid
